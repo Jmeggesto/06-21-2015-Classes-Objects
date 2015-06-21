6562acfb75d759c867ef8d1fe9eee9028bf2f12f
Jackie Meggesto <jackie.meggesto@gmail.com>
2015-06-21 11:13:48 -0400

    added xcode project

---
+++ 06-21-2015 Bank Account Pair Programming/06-21-2015 Bank Account Pair Programming/main.m
@@ -0,0 +1,24 @@
+//
+//  main.m
+//  06-21-2015 Bank Account Pair Programming
+//
+//  Created by Jackie Meggesto on 6/21/15.
+//  Copyright (c) 2015 Jackie Meggesto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+
+
+
+
+// BankAccount needs to have a way of tracking the money the Patron withdraws or deposits, and also of displaying current balance. The patron can add money, subtract money, display their current balance, possibly transfer money from one account to another. Potential additional bonus features: if the patron has multiple accounts, and overdrafts, the difference + a fee is subtracted from their other account.
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+    }
+    return 0;
+}
