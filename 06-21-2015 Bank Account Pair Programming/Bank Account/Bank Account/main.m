a133a890df5e5a3624002ff8ca9f43f62a4d0b59
Kaisha Jones <kaisha.jones@gmail.com>
2015-06-21 15:45:52 -0400

    jackie taking the wheel

--- 06-21-2015 Bank Account Pair Programming/Bank Account/Bank Account/main.m
+++ 06-21-2015 Bank Account Pair Programming/Bank Account/Bank Account/main.m
@@ -8,10 +8,58 @@
 
 #import <Foundation/Foundation.h>
 
+// Bank Account has a balance
+
+@interface bankAccount : NSObject
+
+-(void) 
+
+@end
+
+@implementation bankAccount {
+    
+    int balance;
+}
+
+
+@end
+
+
+
+// Patron must have: 1. A Name 2. An account 3. and A pocket (remaining balance)
+
+@interface patron : NSObject
+-(void) displayRemainingBalance;
+-(void)deposit:(float)n;
+-(void)withdraw:(float)n;
+
+
+
+
+@end
+
+
+@implementation patron {
+    
+   bankAccount *bankAccount
+}
+
+@end
+
+
+
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//        // insert code here...
+//        NSLog(@"Hello, World!");
+        
+        
+        bankAccount *myAccount = [[bankAccount alloc]init];
+        [
+        
+        
     }
     return 0;
 }
